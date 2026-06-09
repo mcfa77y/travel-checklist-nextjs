7530887fef30f60249c0e0855e33d18f79c90a24
Joe Lau <joe@empohealth.com>
2026-06-08 18:41:17 -0700

    feat: configure 3-column flow layout specifically for printed checklists

--- app/page.tsx
+++ app/page.tsx
@@ -121,7 +121,7 @@ export default function SidebarWithContent() {
           onDeleteList={handleDeleteList}
         />
       </aside>
-      <main className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 gap-6 p-6 bg-slate-300 flex-1 overflow-auto print:bg-transparent print:p-0 print:overflow-visible max-w-7xl w-full mx-auto">
+      <main className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 gap-6 p-6 bg-slate-300 flex-1 overflow-auto print:bg-transparent print:p-0 print:overflow-visible print:columns-3 print:gap-8 max-w-7xl w-full mx-auto">
         {checkedItems.map((id) => (
           <div key={id} className="break-inside-avoid mb-6">
             <CheckList
